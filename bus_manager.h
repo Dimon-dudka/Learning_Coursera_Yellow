0f811e0a7ab752f6ba99714f1a4d83614c79ce2a
Dmitrii <62469554+Dimon-dudka@users.noreply.github.com>
2022-03-20 22:36:24 +0300

    Splitting the program into different files
    Decomposition of the program "Bus stops - 1" and splitting into different files.

---
+++ bus_manager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+
+#include"responses.h"
+
+using namespace std;
+
+class BusManager {
+public:
+    void AddBus(const string& bus, const vector<string>& stops);
+
+    BusesForStopResponse GetBusesForStop(const string& stop) const;
+
+    StopsForBusResponse GetStopsForBus(const string& bus) const;
+    
+
+AllBusesResponse GetAllBuses() const;
+
+private:
+    map<string, vector<string>> buses_to_stops;
+    map<string, vector<string>> stops_to_buses;
+};
